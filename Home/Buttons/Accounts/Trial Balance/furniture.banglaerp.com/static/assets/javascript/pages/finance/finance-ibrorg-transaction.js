7d680f623eb8bce692ba1edec71dbb616df88b67
ArTazmir1 <artazmir@gmail.com>
2026-01-06 07:46:40 +0600

    update

---
+++ Home/Buttons/Accounts/Trial Balance/furniture.banglaerp.com/static/assets/javascript/pages/finance/finance-ibrorg-transaction.js
@@ -0,0 +1,85 @@
+$(document).ready(function() {
+    refresh_branch_list('');
+    var global_branch_code = document.getElementById('id_global_branch_code').value;
+    refresh_ledger_list('IBR_TRAN', 'IBR', global_branch_code);
+    $('#id_res_branch_code').val(global_branch_code);
+    $('#id_org_branch_code').val(global_branch_code);
+});
+
+$(window).on('load', function() {
+    var global_branch_code = document.getElementById('id_global_branch_code').value;
+    $('#id_res_branch_code').val(global_branch_code);
+    $('#id_org_branch_code').val(global_branch_code);
+});
+
+function refresh_branch_list(branch_code) {
+    var url = 'appauth-choice-branchlistall';
+    $.ajax({
+        url: url,
+        data: {
+            'branch_code': branch_code
+        },
+        success: function(data) {
+            $("#id_res_branch_code").html(data);
+            $("#id_org_branch_code").html(data);
+        }
+    });
+    return false;
+}
+
+function refresh_ledger_list(transaction_screen, tran_type, branch_code) {
+    var url = '/finance-choice-cashnbanklist';
+    $.ajax({
+        url: url,
+        data: {
+            'tran_screen': transaction_screen,
+            'tran_type': tran_type,
+            'branch_code': branch_code
+        },
+        success: function(data) {
+            $("#id_res_gl_code").html(data);
+        }
+    });
+    return false;
+}
+
+$("#id_org_branch_code").on("change paste keyup", function() {
+    var org_branch_code = document.getElementById('id_org_branch_code').value;
+    refresh_ledger_list('IBR_TRAN', 'IBR', org_branch_code);
+});
+
+$(function() {
+
+    $(function() {
+        $('#btnAddItem').click(function() {
+            post_tran_table_data();
+
+        });
+    });
+
+    function post_tran_table_data() {
+        var data_string = $("#tran_table_data").serialize();
+        var data_url = $("#tran_table_data").attr('data-url');
+        $('#page_loading').modal('show');
+        $.ajax({
+            url: data_url,
+            data: data_string,
+            type: 'POST',
+            dataType: 'json',
+            success: function(data) {
+                if (data.form_is_valid) {
+                    $('#page_loading').modal('hide');
+                    alert(data.success_message);
+                    document.getElementById("tran_table_data").reset();
+                    table_data.ajax.reload();
+                } else {
+                    $('#page_loading').modal('hide');
+                    alert(data.error_message);
+                    table_data.ajax.reload();
+                }
+            }
+        })
+        return false;
+    }
+
+});
